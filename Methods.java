884b5cbb58c4a4ab6311282fb6af6d77798e95d1
Anujdev67 <94992555+Anujdev67@users.noreply.github.com>
2022-10-24 17:55:19 +0530

    Methods.java

---
+++ Methods.java
@@ -0,0 +1,9 @@
+import java.util.Arrays;
+
+public class Methods {
+    public static void main(String[] args) {
+        String name = "Anuj Sen";
+        System.out.println(Arrays.toString(name.toCharArray()));
+        System.out.println(name.indexOf('a'));
+    }
+}
